58b424aa6c7e3d2e0c03d481715719636818ae77
DavidO <dquesada969x@gmail.com>
2021-03-02 18:09:37 -0500

    testing cool parser

--- parser.py
+++ parser.py
@@ -1,25 +1,48 @@
 import sys,argparse
 import os
+import Instructions as ins
+time=0
+# <time> create hub <name> <cantidad de puertos>
+# <time> create host <name>
+# <time> connect <port1> <port2>
+# <time> send <host> <data>
+# <time> disconnect <port>
+
+# def change_global(newval):
+#     @global time = newval
+
+# metodos=[lambda name,ports:ins.Instructions.create_hub(name,ports),ins.Instructions.connect_hub_hub() ]
 
 
+
+dic1 ={"create hub":lambda args=ins.Instructions.create_hub(args[0],args[1]),
+        "create host":lambda args=ins.Instructions.create_hub(args[0],args[1])),
+        "connect":lambda args:ins.Instructions.connect_hub_hub(args[0],args[1]),
+        "send" :lambda args :ins.Instructions.send(args[0],args[1]),
+        "disconnect":lambda args:ins.Instructions.disconnect(args[0])}
+
+def 
 def main():
     # create parser
     
-    # parser =argparse.ArgumentParser(description="Instrucciones del script")
-    # # add a
-    # parser.add_argument('filename',dest='file',required=True)
-    
-    # parser.add_argument('create', dest='imgFile', required=True)
-    # parser.add_argument('connect', dest='scale', required=True)
-    # parser.add_argument('send', dest='scale', required=True)
-    # parse arguments
-    # args = parser.parse_args()
-    # f=args.file
-    f=open("file.txt", 'r')
-    lines=f.readlines()
+    parser =argparse.ArgumentParser(description="Instrucciones del script")
+    parser.add_argument('-f', dest='textfile', default=True)
+    args = parser.parse_args()
+    filename=args.textfile
+    f=open(filename, 'r')
+    for line in f.readlines():
+        codes=line.split(' ')
+        time=codes[0]
+        codes=codes[1:]
+        if code[0]=="create":
+            dic1[codes[0]]+" "+codes[1]](codes[3:])
+        else:
+            dic1[codes[0]](codes[2:])    
+
+        
     # file2=read(f)
     # print("OO")
-    print(lines[0])
+    # print(lines[0])
 
 main()
 
