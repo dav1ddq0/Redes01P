509e28821afd94ccf40926d25636d080ff6e23e4
DavidO <dquesada969x@gmail.com>
2021-03-02 14:37:50 -0500

    tercer commit

--- parser.py
+++ parser.py
@@ -5,19 +5,23 @@ import os
 def main():
     # create parser
     
-    parser =argparse.ArgummentParser(description="Instrucciones del script")
-    # add a
-    parser.add_argument(, dest='file')
+    # parser =argparse.ArgumentParser(description="Instrucciones del script")
+    # # add a
+    # parser.add_argument('filename',dest='file',required=True)
     
     # parser.add_argument('create', dest='imgFile', required=True)
     # parser.add_argument('connect', dest='scale', required=True)
     # parser.add_argument('send', dest='scale', required=True)
     # parse arguments
-    args = parser.parse_args()
-    f=args.file
-    inst=open(f, 'r')
-    print(ins[0])
+    # args = parser.parse_args()
+    # f=args.file
+    f=open("file.txt", 'r')
+    lines=f.readlines()
+    # file2=read(f)
+    # print("OO")
+    print(lines[0])
 
+main()
 
 
 
