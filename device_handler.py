0904166b0402765361a221ed0e1827cae92cc68c
dmguezjaviersnet <dmguezjaviersnet@gmail.com>
2021-03-02 23:26:55 -0500

    renombrado instructions.py a device_handler.py, agregado constructor a la clase y metodos estaticos cambiados a metodos de instancia, agregadas algunas cosas en el parser

--- device_handler.py
+++ device_handler.py
@@ -1,16 +1,21 @@
 import Objs
 
-class Instructions:
-    @staticmethod
-    def create_pc(name):
+class Device_handler:
+    def __init__(self) -> None:
+        self.hubs = []
+        self.hosts = []
+        self.time = 0
+
+    def create_pc(self, name):
         return Objs.Computer(name)
 
-    @staticmethod
-    def create_hub(name, id):
+    def create_hub(self, name, id):
         return Objs.Hub(name, id)
 
-    @staticmethod
-    def connect_pc_pc(pc_1 : Objs.Computer, pc_2 : Objs.Computer):
+    def identify_connection_device(self, device1, device2):
+        tokens = device1.split('_')
+
+    def connect_pc_pc(self, pc_1 : Objs.Computer, pc_2 : Objs.Computer):
         if pc_1.connections[0] != None or pc_2.connections[0] != None:
             return False
 
@@ -18,8 +23,7 @@ class Instructions:
         pc_2.connections.append(pc_1)
         return True
 
-    @staticmethod
-    def connect_hub_hub(hub_1 : Objs.Hub, hub_2 : Objs.Hub,port_hub_1 : int, port_hub_2 : int):
+    def connect_hub_hub(self, hub_1 : Objs.Hub, hub_2 : Objs.Hub,port_hub_1 : int, port_hub_2 : int):
         if hub_1[port_hub_1] != None or hub_2[port_hub_2] != None:
             return False
 
@@ -27,8 +31,7 @@ class Instructions:
         hub_2.connections[port_hub_2] = hub_1  
         return True
     
-    @staticmethod
-    def connect_hub_pc(hub : Objs.Hub, pc : Objs.Computer, hub_port : int):
+    def connect_hub_pc(self, hub : Objs.Hub, pc : Objs.Computer, hub_port : int):
         if hub.connections[hub_port] != None or pc.connections[0] != None:
             return False
 
@@ -36,14 +39,12 @@ class Instructions:
         pc.connections[0] = hub
         return True
 
-    @staticmethod
-    def disconnect_pc(pc : Objs.Computer):
+    def disconnect_pc(self, pc : Objs.Computer):
         pc.connections[0] = None
 
-    @staticmethod
-    def disconnect_hub(hub : Objs.Hub, hub_port : int):
+
+    def disconnect_hub(self, hub : Objs.Hub, hub_port : int):
         hub.connections[hub_port] = None
 
-    @staticmethod
-    def send(origin_pc : Objs.Computer, info):
+    def send(self, origin_pc : Objs.Computer, info):
         pass
--- parser.py
+++ parser.py
@@ -1,13 +1,12 @@
 import sys,argparse
 import os
-import Instructions as ins
-time=0
+import device_handler as dh
+
 # <time> create hub <name> <cantidad de puertos>
 # <time> create host <name>
 # <time> connect <port1> <port2>
 # <time> send <host> <data>
 # <time> disconnect <port>
-
 # def change_global(newval):
 #     @global time = newval
 
@@ -15,29 +14,50 @@ time=0
 
 
 
-dic1 ={"create hub":lambda args=ins.Instructions.create_hub(args[0],args[1]),
-        "create host":lambda args=ins.Instructions.create_hub(args[0],args[1])),
-        "connect":lambda args:ins.Instructions.connect_hub_hub(args[0],args[1]),
-        "send" :lambda args :ins.Instructions.send(args[0],args[1]),
-        "disconnect":lambda args:ins.Instructions.disconnect(args[0])}
 
-def 
+
 def main():
+    handler = dh.Device_handler()
+    dic1 ={"create hub": lambda args :  handler.create_hub(args[0],args[1]),
+        "create host" : lambda args : handler.create_hub(args[0],args[1]),
+        "connect":lambda args : handler.connect_hub_hub(args[0],args[1]),
+        "send" :lambda args : handler.send(args[0],args[1]),
+        "disconnect":lambda args : handler.disconnect(args[0])}
     # create parser
-    
-    parser =argparse.ArgumentParser(description="Instrucciones del script")
+    parser = argparse.ArgumentParser(description="Instrucciones del script")
     parser.add_argument('-f', dest='textfile', default=True)
     args = parser.parse_args()
     filename=args.textfile
-    f=open(filename, 'r')
+    f = open(filename, 'r')
     for line in f.readlines():
-        codes=line.split(' ')
-        time=codes[0]
-        codes=codes[1:]
-        if code[0]=="create":
-            dic1[codes[0]]+" "+codes[1]](codes[3:])
-        else:
-            dic1[codes[0]](codes[2:])    
+        instruction_time = 0
+        codes = line.split(' ')
+
+        try :
+            instruction_time = int(codes[0])
+        except ValueError:
+            print("Invalid parameter")
+
+        codes = codes[1:]
+        if codes[0] == "create":
+            if codes[1] == "hub":
+                ports_amount = 0
+                try :
+                    ports_amount = int(codes[3])
+                except ValueError:
+                    print("Invalid parameter")
+
+                if len(codes) == 4:
+                    dic1[codes[0]+ " " + codes[1]](codes[2:])
+            
+            if codes[1] == "host":
+                if len(codes) == 3:
+                    dic1[codes[0]+ " " + codes[1]](codes[2:])
+
+        elif codes[0] == "connect":
+            if len(codes) == 3 and codes[1].find('_') != -1 and codes[2].find('_') != -1:
+                
+              
 
         
     # file2=read(f)
