a0885ff5d2df2cdc10bc5bbfa3ee8d9c1e7c41d5
DavidO <dquesada969x@gmail.com>
2021-03-03 00:20:16 -0500

    add UpdateFile methods in Objs classes

--- Objs.py
+++ Objs.py
@@ -3,10 +3,23 @@ class Computer:
     def __init__(self, name) -> None:
         self.name = name
         self.connections = [None]*1
-        self.file=open(f"{name}.txt",'w')
-
+        self.file=f"{name}.txt"
+        f=open(self.file,'w')
+        f.close()
+    
+    def UpdateFile(self,name,message):
+        f=open(self.file,'w')
+        f.write(message)
+        f.close()
 class Hub:
     def __init__(self, name, ports_amount : int) -> None:
         self.name = name
         self.connections = [None]*ports_amount
-        self.file=open(f"{name}.txt",'w')
+        self.file=f"{name}.txt"
+        f=open(self.file,'w')
+        f.close()
+
+    def UpdateFile(self,name,message):
+        f=open(self.file,'w')
+        f.write(message)
+        f.close()    
