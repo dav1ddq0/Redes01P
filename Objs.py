e834ef67191e1ef346746058fa9295c94baedef9
DavidO <dquesada969x@gmail.com>
2021-03-03 00:04:13 -0500

    add filel prop

--- Objs.py
+++ Objs.py
@@ -1,9 +1,12 @@
+import os
 class Computer:
     def __init__(self, name) -> None:
         self.name = name
         self.connections = [None]*1
+        self.file=open(f"{name}.txt",'w')
 
 class Hub:
     def __init__(self, name, ports_amount : int) -> None:
         self.name = name
         self.connections = [None]*ports_amount
+        self.file=open(f"{name}.txt",'w')
