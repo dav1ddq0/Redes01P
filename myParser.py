64dd413b535d3e2ea5cd6452c7ca1c1ae0f65e67
DavidO <dquesada969x@gmail.com>
2021-03-05 16:32:41 -0500

    change parser design

--- myParser.py
+++ myParser.py
@@ -1,74 +1,30 @@
-def parse(line):
+caller ={
+        "create" : lambda args : create_parse(args) ,
+        "connect" : lambda args : connect_parse(args),
+        "send" : lambda args : send_parse(args),
+        "disconnect" : lambda args :  disconnect_parse(args)
+        }
+
+# parse de linne of the file
+def parse(line:str):
+    #remove the line jump
     line = line.replace('\n','')
+    #divide the line in tokens
     codes = line.split(' ')
     instruction_time = 0
-
     try :
         instruction_time = int(codes[0])
     except ValueError:
         print("Invalid parameter")
 
     codes = codes[1:]
-    if codes[0] == "create":
-        if codes[1] == "hub":
-            ports_amount = 0
-            try :
-                ports_amount = int(codes[3])
-            except ValueError:
-                 print("Invalid parameter")
 
-            if len(codes) == 4:
-                    return "create hub", codes[2], ports_amount
-            else : print("Invalid amount of arguments")
-            
-        if codes[1] == "host":
-            if len(codes) == 3:
-                return "create host", codes[2]
-            else : print("Invalid amount of arguments")
+    if codes[0] not in caller.keys():
+        print(f"{codes[0]} is invalid command")
+        return
+    else
+        return caller(codes[0])
 
-    elif codes[0] == "connect":
-        if codes[1].find('_') != -1 and codes[2].find('_') != -1:
-            device1_name, device1_port = codes[1].split('_')
-            device2_name, device2_port = codes[2].split('_')
-
-            try:
-                device1_port = int(device1_port)
-                device2_port = int(device2_port)
-            except ValueError:
-                print("Invalid parameters")
-            
-            if len(codes) == 3:
-                return codes[0], codes[1], codes[2]
-            else : print("Invalid amount of arguments")
-            
-
-    elif codes[0] == "disconnect":
-        if codes[1].find('_'):
-            device_name, device_port = codes[1].split('_')
-                
-            try:
-                device_port = int(device_port)
-            except ValueError:
-                print("Invalid parameters")
-
-            if len(codes) == 2: 
-                return codes[0], codes[1]
-            else : print("Invalid amount of arguments")
-    
-    elif codes[0] == "send":
-        if codes[1].find('_'):
-            device_name, device1_port = codes[1].split('_')
-
-            try:
-                device_port = int(device_port)
-            except ValueError:
-                print("Invalid parameters")
-
-            if len(codes) == 3:
-                if __check_binary(codes[2]):
-                    return codes[0], codes[1], codes[2]
-                else : print("The data to send must be a binary code")
-                      
 def __check_binary(string):
     s = set(string)
     p = {0 , 1}
@@ -78,55 +34,72 @@ def __check_binary(string):
 
     return False
 
-#
-#
-## the possible instructions to execute are create,connect,send,disconnect
-#def validInst(inst:str):
-#    return inst != "create" and inst != "connect" and inst != "send" and inst != "disconnect"
-#
-#def create_call(args:list):
-#    if args[0] == "hub":
-#        ports_amount = 0
-#        try :
-#            ports_amount = int(args[2])
-#            name =args[1]
-#            if len(args) == 3:
-#                caller["hub"](name,ports_amount)    
-#        except ValueError:
-#            print("Invalid parameter")
-#    if args[0] == "host":
-#        if len(args) == 2:
-#            name=args[1]
-#            caller["host"](name)
-#        else:
-#            print("Invalid number of args")
-#
-#def connect_call(args):
-#    if len(args) == 2 and args[1].find('_') != -1 and args[2].find('_') != -1:
-#        device1_name, device1_port = args[1].split('_')
-#        device2_name, device2_port = args[2].split('_')
-#        try:
-#            device1_port = int(device1_port)
-#            device2_port = int(device2_port)
-#            handler.connect(device1_port,device2_port)
-#        except ValueError:
-#            print("Invalid parameters")
-#    else :
-#        print("Invalid amount of arguments")
-#
-#def connect_call(args:list):
-#    if len(args) != 2 :
-#        print("Invalid args count ")
-#        
-#    else:
-#        port1=args[0]
-#        port2=args[2]
-#        
-#        
-#
-#def disconnect_call(args:list):
-#    if len(args) != 1:
-#        print("Invalida count of args ")
-#    else:
-#        port=args[0]
-#        handler.disconnect(port)
+def create_parse(args:list):
+    if args[1] == "hub":
+        ports_amount = 0
+        try :
+            ports_amount = int(args[3])
+        except ValueError:
+            print("Invalid parameter")
+        if len(args) == 4:
+                # hub,name,ports_amount
+                return args[1], args[2], ports_amount
+        else : print("Invalid amount of arguments")
+        
+    elif args[1] == "host":
+        if len(args) == 3:
+            return args[1], args[2]
+        else : print("Invalid amount of arguments")
+
+
+def connect_parse(args:list):
+
+    if args[1].find('_') != -1 and args[2].find('_') != -1:
+            device1_name, device1_port = args[1].split('_')
+            device2_name, device2_port = args[2].split('_')
+
+            try:
+                device1_port = int(device1_port)
+                device2_port = int(device2_port)
+            except ValueError:
+                print("Invalid parameters")
+            
+            if len(args) == 3:
+                # connect,port1,port2
+                return args[0], args[1], args[2]
+            else : print("Invalid amount of arguments")
+
+    if len(args) != 2 :
+        print("Invalid args count ")
+
+    else:
+        port1=args[0]
+        port2=args[2]
+
+def send_parse(args:list):
+    if args[1].find('_'):
+        device_name, device1_port = args[1].split('_')
+        try:
+            device_port = int(device_port)
+        except ValueError:
+            print("Invalid parameters")
+        if len(args) == 3:
+            if __check_binary(args[2]):
+                return args[0], args[1], args[2]
+            else : print("The data to send must be a binary code")
+
+# sintax error of insr time disconnect
+def disconnect_parse(args:list):
+    if args[1].find('_'):
+        device_name, device_port = args[1].split('_')
+            
+        try:
+            device_port = int(device_port)
+        except ValueError:
+            print("Invalid parameters")
+        if len(args) == 2: 
+            # disconnect,port
+            return args[0], args[1]
+        else : print("Invalid amount of arguments")
+    else:
+        print("Invalid format")    
